b21f73d423efca7133293fba9e5308972a601c64
Isaac Luo <yishaluo@gmail.com>
2017-11-25 20:17:12 +0000

    fix

--- start_prod.sh
+++ start_prod.sh
@@ -2,4 +2,4 @@ mkdir -p /var/log/cailab-homepage
 LOG=/var/log/cailab-homepage
 PID=/var/log/cailab-homepage/forever.pid
 
-forever -l $LOG/access.log -e $LOG/error.log -o $LOG/out.log -a --pidFile $PID start ./build/app.js
+forever -l $LOG/access.log -e $LOG/error.log -o $LOG/out.log -a --pidFile $PID start ./build/start-backend.js
