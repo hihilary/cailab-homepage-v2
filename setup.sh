fc1e426200d7e2e674c17dceb568ab034f3209d4
Isaac Luo <yishaluo@gmail.com>
2019-09-20 13:46:59 +0100

    remove some script from setup.sh

--- setup.sh
+++ setup.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-SKIP_BUILD=
-while getopts "n" arg
-do
-    case $arg in
-        n)
-            SKIP_BUILD=1
-            ;;
-    esac
-done
+# SKIP_BUILD=
+# while getopts "n" arg
+# do
+#     case $arg in
+#         n)
+#             SKIP_BUILD=1
+#             ;;
+#     esac
+# done
 
-if [ -z "$SKIP_BUILD" ]; then
-    cd frontend
-    yarn install
-    yarn run build
-    cd ../mobile
-    yarn install
-    yarn run build
-    cd ..
-fi
-mkdir -p /srv/cailab-homepage/db
-cp -n ./api-server/db/* /srv/cailab-homepage/db/
+# if [ -z "$SKIP_BUILD" ]; then
+#     cd frontend
+#     yarn install
+#     yarn run build
+#     cd ../mobile
+#     yarn install
+#     yarn run build
+#     cd ..
+# fi
+mkdir -p /shared/cailab-homepage/db
+cp -n ./api-server/db/* /shared/cailab-homepage/db/
 docker-compose up --build -d
